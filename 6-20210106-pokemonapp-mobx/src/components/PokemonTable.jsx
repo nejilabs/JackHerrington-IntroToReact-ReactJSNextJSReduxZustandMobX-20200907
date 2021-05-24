cddef975eba1d14db380dda24ab70c46c59fdd2c
nellyXinwei <nelsonalbajr@yahoo.com>
2021-05-24 12:21:42 +0800

    @e14t1151init

---
+++ 6-20210106-pokemonapp-mobx/src/components/PokemonTable.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import useStore from '../store'
+
+import PokemonRow from "./PokemonRow";
+
+function PokemonTable() {
+  // Start: States
+  const pokemons = useStore(state => state.pokemons)
+  const filter = useStore(state => state.filter)
+  const setSelectedPokemon = useStore(state => state.setSelectedPokemon)
+  // End: States
+
+
+  // START: TEMPLATES
+  return (
+    <table width="100%">
+      <tbody>
+        {pokemons
+          .filter(({ name: { english } }) =>
+            english.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+          )
+          .slice(0, 20)
+          .map((pokemon) => (
+            <PokemonRow
+              pokemon={pokemon}
+              onClick={(pokemon) => setSelectedPokemon(pokemon)}
+            />
+          ))}
+      </tbody>
+    </table>
+  );
+  // END: TEMPLATES
+
+}
+
+export default PokemonTable;
