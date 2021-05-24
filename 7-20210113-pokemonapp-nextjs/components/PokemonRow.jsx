e4c410dd65d0ae335d7fcf20cccbea2b381644b8
nellyXinwei <nelsonalbajr@yahoo.com>
2021-05-24 16:08:33 +0800

    @e15t1301o547migrated

---
+++ 7-20210113-pokemonapp-nextjs/components/PokemonRow.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+
+const PokemonRow = ({ pokemon, onClick }) => (
+  <>
+    <tr>
+      <td>{pokemon.name.english}</td>
+      <td>{pokemon.type.join(", ")}</td>
+      <td>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => onClick(pokemon)}
+        >
+          More Information
+        </Button>
+      </td>
+    </tr>
+  </>
+);
+
+
+export default PokemonRow;
